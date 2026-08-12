99a0e93b317e6f58b058f4f60f73a0fe77aa1c98
dmitriy_kashirin <kda20.02@mail.ru>
2026-08-12 16:28:23 +0300

    Готов к выгрузке на Platform.sh

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c%uyx$5hi_i5_k9vy7b^w_by!*)yftn+-^&lh8-6z3cnqtf^yv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -136,3 +136,28 @@ STATIC_URL = 'static/'
 
 #Мои настройки
 LOGIN_URL = '/users/login/'
+
+#Настройки platform.sh.
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / 'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+            }
+        }
